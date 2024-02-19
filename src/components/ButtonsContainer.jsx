7e673c15c56a83da8ee2d87b42407e4be4359e53
sameer2407 <satyamjadaun2001@gmail.com>
2024-02-20 00:00:55 +0530

    divide button changed

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -1,7 +1,7 @@
 import styles from "../components/ButtonsContainer.module.css"
 
 const ButtonsContainer = ({onButtonClick}) => {
-  const buttonNames = ['c', '1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '=', '0', '.'];
+  const buttonNames = ['c', '1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '=', '0', '.','/'];
 
   return (
     <div className={styles.buttonsContainer}>
